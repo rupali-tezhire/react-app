0f13ce39307042c9b751b961c256bde253b8857d
rupali-tezhire <rupali.khatri@tezhire.com>
2024-10-28 11:30:53 +0530

    test7

--- vite.config.js
+++ vite.config.js
@@ -4,14 +4,4 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    port: 3000,
-    open: true, 
-  },
-  build: {
-    outDir: 'dist',
-    minify: 'terser',
-    sourcemap: true,
-  },
-  base: './', 
 })
